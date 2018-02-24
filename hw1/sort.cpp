4ed9750ce3e8412765cd80728483db088b0f1b5d
Bryant Pham <bryantpham@Bryants-MacBook-Pro.local>
2018-02-24 02:18:43 -0600

    Remove debugging lines from quick sort

--- hw1/sort.cpp
+++ hw1/sort.cpp
@@ -7,7 +7,7 @@
 #include "sorter.h"
 using namespace std;
 
-string* createRecordsFromFile(string& input) {
+string* createRecordsFromFile(string& input, int& size) {
     DoublyLinkedList<string> list;
     ifstream in_file(input);
     string record;
@@ -15,6 +15,7 @@ string* createRecordsFromFile(string& input) {
         list.append(record);
     }
     string* records = list.toArray();
+    size = list.size();
     in_file.close();
     return records;
 }
@@ -38,30 +39,39 @@ void print(string* arr, int n) {
     for (int i = 0; i < n; i++) cout << arr[i] << endl;
 }
 
+void writeToFile(string& out, string* records, int& size) {
+    ofstream out_file(out);
+    for (int i = 0; i < size; i++) {
+        out_file << records[i] << endl;
+    }
+    out_file.close();
+}
+
 int main(int argc, char* argv[]) {
     string input = getArg(argv[1]);
     string attributes = getArg(argv[2]);
     string output = getArg(argv[3]);
     string sortType = getArg(argv[4]);
 
-    string* records = createRecordsFromFile(input);
+    int size = 0;
+    string* records = createRecordsFromFile(input, size);
     Comparator comparator = readAttrKeys(attributes);
-    Sorter::mergeSort(records, 4, comparator);
-    print(records, 4);
 
     if (sortType == "select") {
-
+        Sorter::selectionSort(records, size, comparator);
     } else if (sortType == "insert") {
-
+        Sorter::insertionSort(records, size, comparator);
     } else if (sortType == "quick") {
-
+        Sorter::quickSort(records, size, comparator);
     } else if (sortType == "merge") {
-
+        Sorter::mergeSort(records, size, comparator);
     } else if (sortType == "heap") {
-
+        Sorter::mergeSort(records, size, comparator);
     } else {
         cout << "Invalid sort type" << endl;
     }
+    print(records, size);
+    writeToFile(output, records, size);
 
     return 0;
 }

090bf2157f132531851922d9d69ec9a6642a304a
Bryant Pham <bryantpham@Bryants-MacBook-Pro.local>
2018-03-21 15:17:55 -0500

    hw2/remove test code

--- hw2/infinitearithmetic.cpp
+++ hw2/infinitearithmetic.cpp
@@ -25,20 +25,5 @@ int main(int argc, char* argv[]) {
             continue;
         }
     }
-    //string result1 = svc.evaluate("(+1+2)*(+1000+2000)");
-    //cout << result1 << endl;
-    //string result2 = svc.evaluate("((1+2)*(1000+2000))*(1+10000)");
-    //cout << result2 << endl;
-    //string result3 = svc.evaluate("((-1))-(-1)");
-    //cout << result3 << endl;
-
-    ////string result4 = svc.evaluate("9999999999999999999999/9999999999999999999999");
-    ////cout << result4 << endl;
-
-    //string result5 = svc.evaluate("0*00000000000000000+0000000000000001");
-    //cout << result5 << endl;
-
-    //string result6 = svc.evaluate("(1000000000000000-1)");
-    //cout << result6 << endl;
     return 0;
 }
